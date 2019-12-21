1a4342cfa02ceb29186f485512c21f1eba692f88
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2019-12-21 17:38:00 +0100

    Using a Service for Cross-Component Communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import {Recipe} from './recipe.model';
+import {EventEmitter} from '@angular/core';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     // tslint:disable-next-line:max-line-length
     new Recipe('A test Recipe', 'This is simple test', 'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-09-how-to-shrimp-alfredo%2FHT-Shrimp-Alfredo_103'),
@@ -9,6 +12,6 @@ export class RecipeService {
   ];
 
   getRecipes() {
-    return this.recipes.slice(); //zwraca kopie
+    return this.recipes.slice(); // zwraca kopie
   }
 }
