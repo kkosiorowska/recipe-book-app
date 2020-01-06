c64be3d3c9d55bd5aeae995912ab53114d393b2b
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2020-01-06 12:20:21 +0100

    Resetting the Form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -33,14 +33,16 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       );
   }
 
-    onAddItem(form: NgForm) {
-    const value = form.value;
-    const newIngredient = new Ingredient(value.name, value.amount);
-    if (!this.editMode) {
-        this.slsService.addIngredient(newIngredient);
+    onSubmit(form: NgForm) {
+      const value = form.value;
+      const newIngredient = new Ingredient(value.name, value.amount);
+      if (!this.editMode) {
+          this.slsService.addIngredient(newIngredient);
       } else {
-        this.slsService.updateIngredient(this.editedItemIndex, newIngredient);
+          this.slsService.updateIngredient(this.editedItemIndex, newIngredient);
       }
+      this.editMode = false;
+      form.reset();
     }
 
   ngOnDestroy() {
