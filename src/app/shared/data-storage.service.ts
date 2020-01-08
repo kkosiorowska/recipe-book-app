27f4a63054b63da743387fd57351ca7f6de49459
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2020-01-08 11:49:12 +0100

    Sending PUT Requests to Save Data

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {RecipeService} from '../recipes/recipe.service';
+
+
+@Injectable()
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    return this.http.put('https://ng-recipe-book-37167.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+  }
+}
