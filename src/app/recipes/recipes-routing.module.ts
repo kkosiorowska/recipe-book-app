c28244ea2c4e5481ed5e77a4f791714d76874884
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2020-01-10 13:16:31 +0100

    Restructuring Services to use the Child Injector

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -18,7 +18,10 @@ const recipesRoutes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(recipesRoutes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    AuthGuard
+  ]
 })
 export class RecipesRoutingModule {
 
