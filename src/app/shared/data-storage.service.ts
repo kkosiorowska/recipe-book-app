8cfac7c3828106541d254acc3e870fb44c2ff394
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2020-01-13 17:43:39 +0100

    Http Parameters

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
-import {HttpClient} from '@angular/common/http';
-import { map } from 'rxjs/operators';
 import {AuthService} from '../auth/auth.service';
 
 @Injectable()
@@ -13,9 +14,12 @@ export class DataStorageService {
 
   storeRecipes() {
     const token = this.authService.getToken();
+    // const headers =  new HttpHeaders().set('Authorization',  'xxzxcxcxcxcxc');
 
-    return this.http.put('https://ng-recipe-book-37167.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes(), {
-      observe: 'body'
+    return this.http.put('https://ng-recipe-book-37167.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
+      observe: 'body',
+      params: new HttpParams().set('auth', token)
+      // headers: headers
     });
   }
 
