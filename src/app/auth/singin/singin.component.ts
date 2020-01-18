f8c2dd3cafe5c4452e71503549b00cccb0a56863
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2020-01-18 13:24:45 +0100

    Adding Auth Signin

--- src/app/auth/singin/singin.component.ts
+++ src/app/auth/singin/singin.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
-import {AuthService} from '../auth.service';
+import {Store} from '@ngrx/store';
+
+import * as fromApp from '../../store/app.reducers';
+import * as AuthActions from '../store/auth.actions';
 
 @Component({
   selector: 'app-singin',
@@ -9,7 +12,7 @@ import {AuthService} from '../auth.service';
 })
 export class SinginComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private store: Store<fromApp.AppState>) { }
 
   ngOnInit() {
   }
@@ -17,6 +20,6 @@ export class SinginComponent implements OnInit {
   onSignin(form: NgForm) {
     const email = form.value.email;
     const password = form.value.password;
-    this.authService.signinUser(email, password);
+    this.store.dispatch(new AuthActions.TrySignin({username: email, password}));
   }
 }
