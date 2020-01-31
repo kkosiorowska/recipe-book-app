4d767c6b22fed15293ba73ea46eb799a294c8d11
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2020-01-31 11:41:10 +0100

    Data update

--- src/app/recipes/store/recipe.reducers.ts
+++ src/app/recipes/store/recipe.reducers.ts
@@ -14,21 +14,49 @@ export  interface  State {
 const initialState: State = {
   recipes: [
   new Recipe(
-    'A test Recipe',
-    'This is simple test',
-    'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-09-how-to-shrimp-alfredo%2FHT-Shrimp-Alfredo_103',
+    'Sweet pancakes',
+    'Easy Fluffy Pancakes',
+    'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
     [
-      new Ingredient('Meat', 1),
-      new Ingredient('French Fries', 20)
+      new Ingredient('Egg', 1),
+      new Ingredient('Strawberry', 10),
+      new Ingredient('Blueberry', 5)
     ]),
   new Recipe(
-    'Another Test Recipe',
-    'This is simple test',
-    'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-09-how-to-shrimp-alfredo%2FHT-Shrimp-Alfredo_103',
+    'Beef burger',
+    'The Best Classic Burger Recipe',
+    'https://images.pexels.com/photos/1563045/pexels-photo-1563045.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
     [
       new Ingredient('Meat', 1),
-      new Ingredient('Buns', 2)
-    ])
+      new Ingredient('Buns', 2),
+      new Ingredient('Onion', 1)
+    ]),
+    new Recipe(
+      ' Delicious Sandwich',
+      'Quick Sandwich Recipe',
+      'https://images.pexels.com/photos/566566/pexels-photo-566566.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+      [
+        new Ingredient('Avocado', 1),
+        new Ingredient('Egg', 2),
+        new Ingredient('Bread', 1)
+      ]),
+    new Recipe(
+      'Tomato Soup',
+      'Classic Tomato Soup Recipe',
+      'https://images.pexels.com/photos/539451/pexels-photo-539451.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+      [
+        new Ingredient('Tomato', 5),
+        new Ingredient('Carrot', 3)
+      ]),
+    new Recipe(
+      'Green Pasta',
+      'Pesto Pasta Recipe',
+      'https://images.pexels.com/photos/2090921/pexels-photo-2090921.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      [
+        new Ingredient('Basil', 1),
+        new Ingredient('Pasta', 1),
+        new Ingredient('Garlic', 1)
+      ])
 ]
 };
 export function recipeReducer(state = initialState, action: RecipeActions.RecipeActions) {
